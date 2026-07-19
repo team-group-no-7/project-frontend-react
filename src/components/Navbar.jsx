5096245425ae2bc31765f636da2fab3d9fd83ce1
yashwantpandey <yashwantpandey516@gmail.com>
2026-07-20 01:58:44 +0530

    Refactor Hero, Navbar, and Stats components for improved readability and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,41 @@
-function Navbar(){
+function Navbar() {
 
-    return(
+    return (
+        <header>
+            <div className="container navbar">
 
-<header>
+                <div className="logo">
+                    LearnHub
+                </div>
 
-<div className="container navbar">
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Explore</a>
+                    <a href="#">Categories</a>
+                    <a href="#">Creators</a>
+                    <a href="#">Community</a>
+                    <a href="#">Pricing</a>
+                </nav>
 
-<div className="logo">
-LearnMint
-</div>
+                <div className="search-box">
+                    <span className="search-icon">⌕</span>
+                    <input
+                        type="text"
+                        placeholder="Search resources, creators..."
+                    />
+                </div>
 
-<nav>
+                <div className="buttons">
+                    <button className="login">
+                        Login
+                    </button>
+                    <button className="signup">
+                        Sign Up
+                    </button>
+                </div>
 
-<a href="#">Home</a>
-
-<a href="#">Explore</a>
-
-<a href="#">Categories</a>
-
-<a href="#">Creators</a>
-<a href="#">Community</a>
-
-<a href="#">Pricing</a>
-
-</nav>
- 
-    <div className="search-box">
-          <span className="search-icon">⌕</span>
-          <input
-            type="text"
-            placeholder="Search resources, creators..."
-          />
-          
-    </div>
-
-
-<div className="buttons">
-
-<button className="login">
-
-Login
-
-</button>
-
-<button className="signup">
-
-Sign Up
-
-</button>
-
-</div>
-
-</div>
-
-</header>
-
+            </div>
+        </header>
     )
 
 }
